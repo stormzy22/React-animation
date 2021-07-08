2b94c0e55718bfbeaacb48fcfea0c44025173a2f
stormzy22 <aawweb23@gmail.com>
2021-07-08 12:50:58 +0100

    started

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Lottie from "react-lottie";
+import animationData from "./animation/data";
 
 function App() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
   return (
     <div className="App">
-      Hello World
+      <Lottie options={defaultOptions} height={400} width={400} />
     </div>
   );
 }
