29e75c2cab8be6ccfb48604234f78cc626e0bdc1
stormzy22 <aawweb23@gmail.com>
2021-07-08 12:31:35 +0100

    clear

--- lottiejs/plain-lottiejs/script/index.js
+++
@@ -1,9 +0,0 @@
-var animation = bodymovin.loadAnimation({
-  container: document.getElementById("bm"), // Required
-  renderer: "svg", // Required
-  loop: true, // Optional
-  autoplay: true, // Optional
-  path: "data.json", // Required
-  //   name: "Hello World", // Name for future reference. Optional.
-});
-console.log(bodymovin);
